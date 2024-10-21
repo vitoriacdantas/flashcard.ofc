186941f2b0c078ae72235a9210c951295f4255f6
vitoriacdantas <160750350+vitoriacdantas@users.noreply.github.com>
2024-10-21 11:12:40 -0300

    aula6

--- perguntas.js
+++ perguntas.js
@@ -14,4 +14,10 @@ criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
-)   
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
